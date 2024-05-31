4d930c59fa0a93001efc767840136fe929031f4e
Matthew Mayer <matthew@themayers.org>
2024-05-31 12:10:22 -0700

    Fix possible nil pointer error

--- cmd/j5/main.go
+++ cmd/j5/main.go
@@ -262,6 +262,7 @@ func runProtoBuild(ctx context.Context, cfg struct {
 	} else if cfg.CommitHash == "" || cfg.CommitTime == "" {
 		return fmt.Errorf("commit hash and time are required, or set --git-auto")
 	} else {
+		commitInfo = &builder_j5pb.CommitInfo{}
 		commitInfo.Hash = cfg.CommitHash
 		commitTime, err := time.Parse(time.RFC3339, cfg.CommitTime)
 		if err != nil {
