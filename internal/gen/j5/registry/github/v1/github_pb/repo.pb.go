29fe310f42a9afdecd2a4505e40b1759ba4721e2
Damien Whitten <dw@chaos404.com>
2024-08-08 13:58:43 -0700

    Update Deps

--- internal/gen/j5/registry/github/v1/github_pb/repo.pb.go
+++ internal/gen/j5/registry/github/v1/github_pb/repo.pb.go
@@ -10,7 +10,7 @@ import (
 	_ "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 	_ "github.com/pentops/j5/gen/j5/ext/v1/ext_j5pb"
 	_ "github.com/pentops/j5/gen/j5/list/v1/list_j5pb"
-	psm_pb "github.com/pentops/protostate/gen/state/v1/psm_pb"
+	psm_pb "github.com/pentops/j5/gen/psm/state/v1/psm_pb"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
